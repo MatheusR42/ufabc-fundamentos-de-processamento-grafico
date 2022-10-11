c5f7b42716d9771d552349bc91065480c0217c08
matheusr42 <matheus.araujo.r42@gmail.com>
2022-10-11 01:12:58 -0300

    2

--- 2.cpp
+++ 2.cpp
@@ -78,7 +78,7 @@ int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(250, 250);
+    glutInitWindowSize(400, 300);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Equipe Átomos");
     init();
